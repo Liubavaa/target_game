1877344935aeeb84c5746a8c2dc0c2bd17c79466
Liubavaa <liubomyr.oleksiuk@ucu.edu.ua>
2021-10-30 14:26:44 +0300

    made list with word with central letter

--- target_game.py
+++ target_game.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
@@ -11,9 +12,16 @@ def generate_grid() -> List[List[str]]:
 def get_words(f: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
+    >>> get_words('en.txt', ['e', 'm', 'x', 'p', 'w', 'z', 'w', 'p', 'i'])
+    []
     """
-    pass
-
+    letter = letters[4]
+    word_center = []
+    with open(f, "r") as dictionary:
+        for word in dictionary:
+            if letter in word and len(word) > 3:
+                word_center.append(word)
+    print(word_center)
 
 
 def get_user_words() -> List[str]:
