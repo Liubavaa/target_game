5aa03bd1c450ab5c493756e6e3dc2b72d34fab5a
Liubavaa <liubomyr.oleksiuk@ucu.edu.ua>
2021-11-04 22:16:39 +0200

    bug with opened file fixed

--- target_ua.py
+++ target_ua.py
@@ -24,19 +24,32 @@ def generate_grid() -> List[List[str]]:
 
 def get_words(path: str, letters: List[str]):
     """
-    Reads the file and checks the words with rules
-    and returns a list of words and their part of language.
-    >>> get_words('https://raw.githubusercontent.com/brown-uk/dict_uk/master/data/dict/base.lst',\
-                    ['й', 'й', 'й', 'й', 'й'])
-    [('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'), ('йодат', 'noun'),\
- ('йодид', 'noun'), ('йодил', 'noun'), ('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'),\
- ('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'),\
- ('йорк', 'noun'), ('йот', 'noun'), ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb')]
+    Reads the file and checks the words with rules and returns a list of words and their part of language.
+    >>> get_words("base.lst", ['й', 'й', 'й', 'й', 'й'])
+    [('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'),\
+ ('йодат', 'noun'), ('йодид', 'noun'), ('йодил', 'noun'), ('йодит', 'noun'),\
+ ('йодль', 'noun'), ('йола', 'noun'), ('йолоп', 'noun'), ('йомен', 'noun'),\
+ ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'), ('йот', 'noun'), ('йота', 'noun'),\
+ ('йти', 'verb'), ('йтися', 'verb')]
+    >>> get_words("base.lst", ['й', 'є', 'ю'])
+    [('євнух', 'noun'), ('єврей', 'noun'), ('євро', 'noun'), ('єгер', 'noun'), ('єдваб', 'noun'), ('єзуїт',\
+ 'noun'), ('єлей', 'noun'), ('ємний', 'adjective'), ('ємно', 'adverb'), ('єна', 'noun'), ('єнот', 'noun'),\
+ ('єпарх', 'noun'), ('єресь', 'noun'), ('єри', 'noun'), ('єрик', 'noun'), ('єрик', 'noun'), ('єство', 'noun'),\
+ ('єті', 'noun'), ('єхида', 'noun'), ('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'),\
+ ('йодат', 'noun'), ('йодид', 'noun'), ('йодил', 'noun'), ('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'),\
+ ('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'), ('йот', 'noun'),\
+ ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb'), ('юань', 'noun'), ('юга', 'noun'), ('югурт', 'noun'),\
+ ('юда', 'noun'), ('юдей', 'noun'), ('юдин', 'adjective'), ('юдоль', 'noun'), ('юзом', 'adverb'), ('юїтка',\
+ 'noun'), ('юка', 'noun'), ('юкола', 'noun'), ('юнак', 'noun'), ('юнга', 'noun'), ('юний', 'adjective'), ('юніор',\
+ 'noun'), ('юнка', 'noun'), ('юнкер', 'noun'), ('юнкор', 'noun'), ('юннат', 'noun'), ('юнь', 'noun'), ('юпка',\
+ 'noun'), ('юра', 'noun'), ('юрба', 'noun'), ('юрик', 'noun'), ('юрист', 'noun'), ('юрма', 'noun'), ('юрода',\
+ 'noun'), ('юрок', 'noun'), ('юрок', 'noun'), ('юрта', 'noun'), ('юрфак', 'noun'), ('юс', 'noun'), ('ют', 'noun'),\
+ ('ютуб', 'noun'), ('юферс', 'noun'), ('юхта', 'noun'), ('юшити', 'verb'), ('юшка', 'noun'), ('ююба', 'noun')]
     """
     result = []
     with open(path, 'r', encoding='UTF-8') as total_txt:
         whole_list = re.findall(
-            r'\n\w+ /n|\n\w+ /v|\n\w+ /adj|\n\w+ adv|\n\w+ n|\n\w+ v|\n\w+ adj|\n\w+ noun',
+            r'\n\w+ /n|\n\w+ /v|\n\w+ /adj|\n\w+ adv|\n\w+ n[^o]|\n\w+ v|\n\w+ adj|\n\w+ noun',
             total_txt.read()
         )
         for line in whole_list:
@@ -57,12 +70,7 @@ def get_words(path: str, letters: List[str]):
 def check_user_words(user_words, language_part, letters, dict_of_words):
     """
     returns list of right user's words and missed right words
-    >>> check_user_words(['йти', 'йог', 'йога', 'гора'], 'verb', ['й', 'й', 'й', 'й', 'й'],\
-    [('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'),('йод', 'noun'),\
-    ('йодат', 'noun'), ('йодид', 'noun'),('йодил', 'noun'), ('йодит', 'noun'),\
-    ('йодль', 'noun'), ('йола', 'noun'), ('йолоп', 'noun'), ('йомен', 'noun'),\
-    ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'), ('йорк', 'noun'), ('йот', 'noun'),\
-    ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb')])
+    >>> check_user_words(['йти', 'йог', 'йога', 'гора'], 'verb', ['й'], get_words("base.lst", ['й']))
     (['йти'], ['йняти', 'йтися'])
     """
     letters += 's'   # рядок просто, щоб використати летерс і не збільшувати кількість перевірок
