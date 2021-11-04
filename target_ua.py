ddf2312d955b2beaca8704f23589a8b816f90054
Liubavaa <liubomyr.oleksiuk@ucu.edu.ua>
2021-11-04 19:23:29 +0200

    pylint 10/10

--- target_ua.py
+++ target_ua.py
@@ -1,3 +1,7 @@
+"""
+part of game to guess words that start with one of five letter
+and is in corresponding part of language
+"""
 from typing import List
 import random
 import urllib.request
@@ -9,8 +13,8 @@ def generate_grid() -> List[List[str]]:
     Generates list of 5 letters - i.e. grid for the game.
     e.g. ['п', 'о', 'м', 'и', 'т']
     """
-    alp = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
-           'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я', 'є', 'і', 'ї', 'ґ']
+    alp = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
+           'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я', 'є', 'і', 'ї', 'ґ']
     grid = []
     while len(grid) < 5:
         char = random.choice(alp)
@@ -21,18 +25,21 @@ def generate_grid() -> List[List[str]]:
 
 def get_words(path: str, letters: List[str]):
     """
-    Reads the file and checks the words with rules and returns a list of words and their part of language.
+    Reads the file and checks the words with rules
+    and returns a list of words and their part of language.
     >>> get_words('https://raw.githubusercontent.com/brown-uk/dict_uk/master/data/dict/base.lst',\
                     ['й', 'й', 'й', 'й', 'й'])
-    [('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'), ('йодат', 'noun'), ('йодид', 'noun'),\
- ('йодил', 'noun'), ('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'), ('йолоп', 'noun'), ('йомен', 'noun'),\
- ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'), ('йорк', 'noun'), ('йот', 'noun'), ('йота', 'noun'),\
- ('йти', 'verb'), ('йтися', 'verb')]
+    [('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'), ('йодат', 'noun'),\
+ ('йодид', 'noun'), ('йодил', 'noun'), ('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'),\
+ ('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), ('йорж', 'noun'),\
+ ('йорк', 'noun'), ('йот', 'noun'), ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb')]
     """
     result = []
     with urllib.request.urlopen(path) as total_txt:
-        whole_list = re.findall(r"\n\w+ /n|\n\w+ /v|\n\w+ /adj|\n\w+ adv|\n\w+ n|\n\w+ v|\n\w+ adj|\n\w+ noun",
-                                total_txt.read().decode('utf8'))
+        whole_list = re.findall(
+            r'\n\w+ /n|\n\w+ /v|\n\w+ /adj|\n\w+ adv|\n\w+ n|\n\w+ v|\n\w+ adj|\n\w+ noun',
+            total_txt.read().decode('utf8')
+        )
         for line in whole_list:
             word = line.split()[0]
             if len(word) < 6:
